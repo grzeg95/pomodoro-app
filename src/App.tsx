38decbcf114e18e2dcd08c8141f9d915ee83e476
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-09-25 15:09:10 +0200

    feat: add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import styles from './App.module.scss';
+import iconLogo from './assets/logo.svg';
 
 export function App() {
 
   return (
-    <div className={styles['container']}></div>
+    <div className={styles['container']}>
+
+      <img src={iconLogo} alt='Pomodoro'/>
+    </div>
   )
 }
